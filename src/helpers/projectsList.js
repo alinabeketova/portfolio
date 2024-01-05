bacb30b66645c019e29876138cbac6995e20e116
Alina <beketova2007@bk.ru>
2024-01-05 23:42:37 +0300

    portfolio

---
+++ src/helpers/projectsList.js
@@ -0,0 +1,50 @@
+import project02 from "./../img/projects/02.png";
+
+import project03 from "./../img/projects/03.png";
+
+import project05 from "./../img/projects/05.png";
+
+import project06 from "./../img/projects/06.png";
+
+import project01 from "./../img/projects/01.png";
+
+
+const projects = [
+	{
+		title: 'Shoppy (charts)',
+		skills: 'React, JavaScript, HTML, Css',
+		img: project01,
+		imgBig: project01,
+		gitHubLink: 'https://alinabeketova.github.io/shoppy-charts/',
+	},
+	{
+		title: 'Avito',
+		img: project02,
+		imgBig: project02,
+		skills: 'JavaScript, HTML, Css',
+		gitHubLink: 'https://alinabeketova.github.io/avito_advertisement_page/',
+	},
+	{
+		title: 'Guitar shop',
+		img: project03,
+		imgBig: project03,
+		skills: 'JavaScript, HTML, Css',
+		gitHubLink: 'https://alinabeketova.github.io/shop-------------/',
+	},
+	{
+		title: 'Landing',
+		img: project05,
+		imgBig: project05,
+		skills: 'HTML, Css, JavaScript',
+		gitHubLink: 'https://alinabeketova.github.io/to-do-list/',
+	},
+	{
+		title: 'Gaming community',
+		img: project06,
+		imgBig: project06,
+		skills: 'HTML, Css, JavaScript',
+		gitHubLink: 'https://alinabeketova.github.io/Alivio/',
+	},
+];
+
+export {projects}
